41e4a88dc7be86f8d2a2f82fa92a11bc65900abb
glowredman <35727266+glowredman@users.noreply.github.com>
2021-01-06 01:58:26 +0100

    Add EventHandler

---
+++ src/main/java/glowredman/fpsp/handler/FPSPEventHandler.java
@@ -0,0 +1,15 @@
+package glowredman.fpsp.handler;
+
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import mods.railcraft.common.worldgen.SulfurGenerator;
+import net.minecraftforge.event.terraingen.OreGenEvent.GenerateMinable;
+
+public class FPSPEventHandler {
+
+	@SubscribeEvent
+	public void onGenerateMinable(GenerateMinable event) {
+		if (event.world.provider.dimensionId == 7 && event.type == SulfurGenerator.EVENT_TYPE)
+			event.setCanceled(true);
+	}
+
+}
--- src/main/java/glowredman/fpsp/proxy/CommonProxy.java
+++ src/main/java/glowredman/fpsp/proxy/CommonProxy.java
@@ -3,13 +3,16 @@ package glowredman.fpsp.proxy;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import glowredman.fpsp.FPSP;
+import glowredman.fpsp.handler.FPSPEventHandler;
 import glowredman.fpsp.world.FPSPWorldGenerator;
+import net.minecraftforge.common.MinecraftForge;
 
 public class CommonProxy {
 
 	public void preInit(FMLPreInitializationEvent event) {
 		GameRegistry.registerItem(FPSP.ITEM, FPSP.ITEM.getUnlocalizedName());
 		GameRegistry.registerWorldGenerator(new FPSPWorldGenerator(), 0);
+		MinecraftForge.EVENT_BUS.register(new FPSPEventHandler());
 	}
 
 }

1fe931bdbad09c9fe392928131711678882996e0
glowredman <fredcraft00@gmail.com>
2021-03-22 22:30:22 +0100

    Add some oreDict entries

--- src/main/java/glowredman/fpsp/handler/OreDictHandler.java
+++ src/main/java/glowredman/fpsp/handler/OreDictHandler.java
@@ -4,9 +4,12 @@ import glowredman.fpsp.Utils;
 import net.minecraftforge.oredict.OreDictionary;
 
 public class OreDictHandler {
-	
+
 	public static void init() {
 		OreDictionary.registerOre("oreHeeStardust", Utils.getItem("HardcoreEnderExpansion", "sphalerite", 1));
+		OreDictionary.registerOre("itemSkull", Utils.getItem("HardcoreEnderExpansion", "enderman_head"));
+		OreDictionary.registerOre("skullEnderman", Utils.getItem("HardcoreEnderExpansion", "enderman_head"));
+		OreDictionary.registerOre("obsidianRod", Utils.getItem("HardcoreEnderExpansion", "obsidian_rod"));
 	}
 
 }

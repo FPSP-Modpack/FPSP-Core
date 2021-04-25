e95cc060ab38af0b653a8f010ce426f3d2a81c90
glowredman <fredcraft00@gmail.com>
2021-04-25 20:02:28 +0200

    Fix red sandstone slab

--- src/main/java/glowredman/fpsp/block/BlockRedSandstoneSlab.java
+++ src/main/java/glowredman/fpsp/block/BlockRedSandstoneSlab.java
@@ -8,7 +8,10 @@ import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Facing;
 import net.minecraft.util.IIcon;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 
 public class BlockRedSandstoneSlab extends BlockSlab {
 
@@ -46,6 +49,72 @@ public class BlockRedSandstoneSlab extends BlockSlab {
 	@Override
 	public String func_150002_b(int p_150002_1_) {
 		return getUnlocalizedName();
+	}
+
+	@Override
+	public void setBlockBoundsBasedOnState(IBlockAccess blockAccess, int x, int y, int z) {
+		if (this.field_150004_a) {
+			this.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
+		} else {
+			if ((blockAccess.getBlockMetadata(x, y, z) % 2) != 0) {
+				this.setBlockBounds(0.0F, 0.5F, 0.0F, 1.0F, 1.0F, 1.0F);
+			} else {
+				this.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 0.5F, 1.0F);
+			}
+		}
+	}
+
+	@Override
+	public int damageDropped(int meta) {
+		return 0;
+	}
+
+	@Override
+	public int onBlockPlaced(World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ, int meta) {
+		return this.field_150004_a ? meta : (side != 0 && (side == 1 || hitY <= 0.5f) ? meta : meta | 2);
+	}
+
+	@Override
+	public boolean shouldSideBeRendered(IBlockAccess blockAccess, int x, int y, int z, int side) {
+		if (this.field_150004_a) {
+			return superShouldSideBeRendered(blockAccess, x, y, z, side);
+		} else if (side != 1 && side != 0 && !superShouldSideBeRendered(blockAccess, x, y, z, side)) {
+			return false;
+		} else {
+			int i1 = x + Facing.offsetsXForSide[Facing.oppositeSide[side]];
+			int j1 = y + Facing.offsetsYForSide[Facing.oppositeSide[side]];
+			int k1 = z + Facing.offsetsZForSide[Facing.oppositeSide[side]];
+			boolean flag = (blockAccess.getBlockMetadata(i1, j1, k1) % 2) != 0;
+			return flag
+					? (side == 0 ? true
+							: (side == 1 && superShouldSideBeRendered(blockAccess, x, y, z, side) ? true
+									: blockAccess.getBlock(x, y, z) != FPSP.blockRedSandstoneSlab
+											|| (blockAccess.getBlockMetadata(x, y, z) % 2) == 0))
+					: (side == 1 ? true
+							: (side == 0 && superShouldSideBeRendered(blockAccess, x, y, z, side) ? true
+									: blockAccess.getBlock(x, y, z) != FPSP.blockRedSandstoneSlab
+											|| (blockAccess.getBlockMetadata(x, y, z) % 2) != 0));
+		}
+	}
+
+	@Override
+	public int getDamageValue(World world, int x, int y, int z) {
+		return 0;
+	}
+
+	@Override
+	public Item getItem(World world, int x, int y, int z) {
+		return Item.getItemFromBlock(FPSP.blockRedSandstoneSlab);
+	}
+
+	private boolean superShouldSideBeRendered(IBlockAccess blockAccess, int x, int y, int z, int side) {
+		return side == 0 && this.minY > 0.0D ? true
+				: (side == 1 && this.maxY < 1.0D ? true
+						: (side == 2 && this.minZ > 0.0D ? true
+								: (side == 3 && this.maxZ < 1.0D ? true
+										: (side == 4 && this.minX > 0.0D ? true
+												: (side == 5 && this.maxX < 1.0D ? true
+														: !blockAccess.getBlock(x, y, z).isOpaqueCube())))));
 	}
 
 }

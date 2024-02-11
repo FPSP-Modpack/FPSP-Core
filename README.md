edd6dc8ef7cf9f52e32acefc2f3dcae470595917
glowredman <35727266+glowredman@users.noreply.github.com>
2024-02-11 22:21:19 +0100

    Update HealthBarRenderer

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # FPSP-Core
-
-[Better healthbar rendering](https://github.com/FPSP-Modpack/FPSP-Core/blob/main/src/main/java/glowredman/fpsp/handler/ClientEventHandler.java) is taken from [Tinker's Construct](https://github.com/SlimeKnights/TinkersConstruct/tree/1.7.10) by mDiyo et al ([License](https://github.com/SlimeKnights/TinkersConstruct#licenses)). All credits to the original authors.
--- src/main/java/glowredman/fpsp/proxy/ClientProxy.java
+++ src/main/java/glowredman/fpsp/proxy/ClientProxy.java
@@ -1,19 +1,24 @@
 package glowredman.fpsp.proxy;
 
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import fox.spiteful.avaritia.render.FancyHaloRenderer;
 import glowredman.fpsp.FPSP;
-import glowredman.fpsp.handler.ClientEventHandler;
 import net.minecraftforge.client.MinecraftForgeClient;
 import net.minecraftforge.common.MinecraftForge;
+import tconstruct.client.HealthBarRenderer;
 
 public class ClientProxy extends CommonProxy {
 
 	@Override
 	public void init(FMLInitializationEvent event) {
 		super.init(event);
+		
 		MinecraftForgeClient.registerItemRenderer(FPSP.itemSingularity, new FancyHaloRenderer());
-		MinecraftForge.EVENT_BUS.register(new ClientEventHandler());
+		
+		final HealthBarRenderer healthBarRenderer = new HealthBarRenderer();
+		MinecraftForge.EVENT_BUS.register(healthBarRenderer);
+		FMLCommonHandler.instance().bus().register(healthBarRenderer);
 	}
 
 }
